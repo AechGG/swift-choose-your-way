596e003e8621044c6b035a02a223b040e83f11b3
AechGG <hazg@me.com>
2020-08-06 11:28:57 +0100

    Initial Commit

---
+++ ChooseYourWay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChooseYourWay
+//
+//  Created by Harrison Gittos on 06/08/2020.
+//  Copyright © 2020 Harrison Gittos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
