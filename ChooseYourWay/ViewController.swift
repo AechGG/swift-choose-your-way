79b8039846be57ff6e113c2720a45843f0a69091
AechGG <hazg@me.com>
2020-08-06 11:47:37 +0100

    feat: some basic setup

--- ChooseYourWay/ViewController.swift
+++ ChooseYourWay/ViewController.swift
@@ -1,18 +1,22 @@
 //
 //  ViewController.swift
-//  ChooseYourWay
+//  Destini-iOS13
 //
-//  Created by Harrison Gittos on 06/08/2020.
-//  Copyright © 2020 Harrison Gittos. All rights reserved.
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
